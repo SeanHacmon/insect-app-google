5b30d5ce25d86f880f57d82b65347e8cdca4a51b
SeanHacmon <seanhacmon@gmail.com>
2024-06-22 13:56:44 +0300

    new branch

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useEffect, useState } from 'react';
+import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
 
 export default function App() {
+  const [error, setError] = useState();
+  const [userInfo, setUserInfo] = useState();
+  useEffect(() => {
+    GoogleSignin.configure();
+  }, []);
+
+  const signin = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const user = await GoogleSignin.signIn();
+      setUserInfo(user);
+      setError();
+    } catch (e) {
+      console.log('Error during sign-in:', e); // Add this line
+      setError(e);
+    }
+  };
+
+  const logout = () => {
+    setUserInfo();
+    GoogleSignin.revokeAccess();
+    GoogleSignin.signOut();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {error && <Text>{"could not log in"}</Text>}
+      {error && <Text>{JSON.stringify(error)}</Text>}
+      {userInfo && <Text>{JSON.stringify(userInfo)}</Text>}
+      {userInfo ? (
+        <Button title="Logout" onPress={logout} />
+      ) : (
+        <GoogleSigninButton
+          size={GoogleSigninButton.Size.Standard}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={signin}
+        />
+      )}
       <StatusBar style="auto" />
     </View>
   );
--- app.config.js
+++ app.config.js
@@ -1,4 +1,4 @@
-{
+export default {
   "expo": {
     "name": "insect-app",
     "slug": "insect-app",
@@ -11,6 +11,9 @@
       "resizeMode": "contain",
       "backgroundColor": "#ffffff"
     },
+    "plugins": [
+      "@react-native-google-signin/google-signin"
+    ],
     "ios": {
       "supportsTablet": true
     },
@@ -18,10 +21,17 @@
       "adaptiveIcon": {
         "foregroundImage": "./assets/adaptive-icon.png",
         "backgroundColor": "#ffffff"
-      }
+      },
+      "package": "com.hacmons.insectapp",
+      "googleServicesFile": process.env.GOOGLE_SERVICES_JSON
     },
     "web": {
       "favicon": "./assets/favicon.png"
+    },
+    "extra": {
+      "eas": {
+        "projectId": "c681f0c7-3cc4-4441-82f7-0387056e7e77"
+      }
     }
   }
 }
